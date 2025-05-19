5bc5da6fd66418b864dd588000c617b2baf3bbe1
rashid <rashidrizan8@gamil.com>
2025-05-19 23:43:32 +0530

    service methods are implemnted

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') // /users
+export class UsersController {
+  /*
+       GET /users
+       GET  /users/:id
+       POST  /user
+       PATCH /users/:id
+       DELETE /users/:id
+    */
+
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id')
+  findUserById(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // post /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
